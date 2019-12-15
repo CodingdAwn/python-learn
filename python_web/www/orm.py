a2201dd6ee994fffc218f7805b8281fc58f2e914
CodingdAwn <gaoxifengfordawn@gmail.com>
2019-12-15 23:44:53 +0800

    mv app.py & some orm.py code

---
+++ python_web/www/orm.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author = 'dAwn_'
+
+import asyncio, logging
+
+import aiomysql
+
+@asyncio.coroutine
+def create_pool(loop, **kw):
+    logging.info('create database connection pool...')
+    global __pool
+    '''
+    __pool = await aiomysql.create_pool(
+        kw.get('maxsize', 10),
+        kw.get('minsize'),
+        False,
+        1,
+        loop,
+        kw
+        )
+    '''
+    __pool = yield from aiomysql.create_pool(
+        host = kw.get('host', 'localhost'),
+        port = kw.get('port', 3306),
+        user = kw['user'],
+        password = kw['password'],
+        db = kw['db'],
+        charset = kw.get('charset', 'utf-8'),
+        autocommit = kw.get('autocommit', True),
+        maxsize = kw.get('maxsize', 10),
+        minsize = kw.get('minsize', 1),
+        loop = loop
+    )
