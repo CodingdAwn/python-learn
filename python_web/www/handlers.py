48621ef0261c224a47f905a744ba5d76369030e9
CodingdAwn <gaoxifengfordawn@gmail.com>
2020-01-31 22:33:25 +0800

    python web day 8

--- python_web/www/handlers.py
+++ python_web/www/handlers.py
@@ -13,9 +13,19 @@ from models import User, Comment, Blog, next_id
 
 @get('/')
 async def index(request):
-    findUsers = await User.findAll()
+#    findUsers = await User.findAll()
+#    return {
+#        # 教程这里是template 而在app.py中调用时却使用的templating
+#        '__templating__': 'test.html',
+#        'users': findUsers
+#    }
+    summary = 'Some text'
+    blogs = [
+        Blog(id = '1', name = 'Test Blog', summary = summary, created_at = time.time() - 120),
+        Blog(id = '2', name = 'something New', summary = summary, created_at = time.time() - 3600),
+        Blog(id = '3', name = 'Learn Swift', summary = summary, created_at = time.time() - 7200)
+    ]
     return {
-        # 教程这里是template 而在app.py中调用时却使用的templating
-        '__templating__': 'test.html',
-        'users': findUsers
+        '__template__': 'blogs.html',
+        'blogs': blogs
     }
